5eb584f80bf3c31090c4a61ff0ec0b2678cdb5a1
Lynus Phua <lynus.phua.2024@computing.smu.edu.sg>
2025-09-11 18:46:33 +0800

    Added simple validation for users

--- src/main/java/com/lynus/cs203/dtos/request/ChangePasswordRequest.java
+++ src/main/java/com/lynus/cs203/dtos/request/ChangePasswordRequest.java
@@ -1,9 +1,15 @@
 package com.lynus.cs203.dtos.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ChangePasswordRequest {
+    @NotBlank(message = "Old password is required")
     private String oldPassword;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 25, message = "Password must be between 6 and 25 characters")
     private String newPassword;
 }
--- src/main/java/com/lynus/cs203/dtos/request/UpdateUserRequest.java
+++ src/main/java/com/lynus/cs203/dtos/request/UpdateUserRequest.java
@@ -1,9 +1,17 @@
 package com.lynus.cs203.dtos.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UpdateUserRequest {
-    public String name;
-    public String email;
+    @NotBlank(message = "Name is required")
+    @Size(max = 255, message = "Name must be at most 255 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
 }
