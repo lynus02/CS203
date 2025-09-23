f91d772bde6c2abc174d42e2e3d8a9f07cdf8f2c
Tan Zhao Yun <zhaoyun.tan.2024@computing.smu.edu.sg>
2025-09-23 22:51:00 +0800

    Add tax calculation service and controller

--- src/main/java/com/lynus/cs203/controllers/TariffController.java
+++ src/main/java/com/lynus/cs203/controllers/TariffController.java
@@ -1,7 +1,10 @@
 package com.lynus.cs203.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TariffController {
@@ -11,4 +14,20 @@ public class TariffController {
         // return the view name (without ".html")
         return "tariff_form";
     }
+
+    @PostMapping("/calculate-tariff")
+    public String calculateTariff(
+            @RequestParam("hsDescription") String hsDescription,
+            @RequestParam("hsCode") String hsCode,
+            @RequestParam("cifValue") double cifValue,
+            @RequestParam("tariffRate") double tariffRate,
+            @RequestParam("vatRate") double vatRate,
+            Model model){
+
+        // tariff calculation // to be done by zhao yun
+
+        return "tariff_result"; // html to show result of calculated tariff
+    }
+
+
 }
