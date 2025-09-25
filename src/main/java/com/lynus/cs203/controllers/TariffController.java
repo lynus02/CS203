dd9e30488b71b8167a1c6a865e8cb5a0cbbe8197
Tan Zhao Yun <zhaoyun.tan.2024@computing.smu.edu.sg>
2025-09-26 01:33:04 +0800

    Revert "added tariff calculation logic"
    This reverts commit 3127798fd7c2e31f2f941a78390e64298573fd10.

--- src/main/java/com/lynus/cs203/controllers/TariffController.java
+++ src/main/java/com/lynus/cs203/controllers/TariffController.java
@@ -1,22 +1,13 @@
 package com.lynus.cs203.controllers;
 
-import com.lynus.cs203.services.TariffCalculationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Controller
 public class TariffController {
-
-    private final TariffCalculationService tariffCalculationService;
-    public TariffController(TariffCalculationService tariffCalculationService) {
-        this.tariffCalculationService = tariffCalculationService;
-    }
 
     @GetMapping("/tariff-form")
     public String showTariffForm() {
@@ -25,19 +16,18 @@ public class TariffController {
     }
 
     @PostMapping("/calculate-tariff")
-    public Map<String, Object> calculate(@RequestParam Integer productCode,
-                                         @RequestParam String countryCode,
-                                         @RequestParam double customsValue) {
-        double tariffAmount = tariffCalculationService.calculateTariff(productCode, countryCode, customsValue);
+    public String calculateTariff(
+            @RequestParam("hsDescription") String hsDescription,
+            @RequestParam("hsCode") String hsCode,
+            @RequestParam("cifValue") double cifValue,
+            @RequestParam("tariffRate") double tariffRate,
+            @RequestParam("vatRate") double vatRate,
+            Model model){
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("product", productCode);
-        response.put("country", countryCode);
-        response.put("customsValue", customsValue);
-        response.put("tariffAmount", tariffAmount);
+        // tariff calculation // to be done by zhao yun
 
-        return response;
 
+        return "tariff_result"; // html to show result of calculated tariff
     }
 
 
