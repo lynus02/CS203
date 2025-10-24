0bd882762d61d03dad434e6aa1935c9b810dcb93
Lynus Phua <lynus.phua.2024@computing.smu.edu.sg>
2025-10-25 00:08:18 +0800

    Fixed creation of new tables

--- src/main/resources/db/migration/V5__Add__TradeAgreement_Table.sql
+++ src/main/resources/db/migration/V5__Add__TradeAgreement_Table.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS trade_agreement (
+CREATE TABLE trade_agreement (
     agreement_id BIGINT PRIMARY KEY AUTO_INCREMENT,
     agreement_name VARCHAR(500) NOT NULL,
     agreement_type VARCHAR(100),
     status VARCHAR(50) DEFAULT 'In Force'
     );
 
-CREATE TABLE IF NOT EXISTS agreement_country (
+CREATE TABLE agreement_country (
     agreement_country_id BIGINT PRIMARY KEY AUTO_INCREMENT,
     agreement_id BIGINT,
     country_name CHAR(255),
